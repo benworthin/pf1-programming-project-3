4d5f02276394d307e692570417b1ec12d9d67f07
Benjamin Worthington <benworthin@icloud.com>
2020-09-18 23:52:47 -0500

    Laid out and wrote TODOs

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // TODO: Number of man hours spent digging in a week
+    //  How many people do you want hired to dig? (1-5)
+    //  Decide if its a cold or hot week for # of hours worked
+    //  20 if hot, 30 if cold (coin flip)
+    //  Use this to calculate how many hours spent digging a week
+
+    // TODO: Number of game cartridges found in a week
+    //  How many game cartridges are found for each man hour of digging
+    //  Roll a dice (1-6) to decide
+    //  Use this to calculate how many found in a week
+
+    // TODO: Sales price of the game cartridges found in a week
+    //  If you sell 1-99 games, $10/game
+    //  100-199 games, ($20 - count/10)
+    //  >=200 games, $1/game
+    //  Use this to calculate money made that week based on games found
+
+    // TODO: Profit/loss for that week
+    //  User at start should enter wage for diggers ($9/hr - $35/hr)
+    //  Calculate the profit/loss with number of man hours, number of games, and sale price
+
+    // TODO: Profit/loss for the year
+    //  Using the numbers above calculate the profit/loss for the year
+
+    return 0;
+
+}
